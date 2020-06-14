f78d3bb298bedf66367a117e8fd53f78c1bd3d1f
Rocky Gray <rocky.grayjr@gmail.com>
2020-06-14 11:45:42 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Buildkite
+
+Testing out Buildkite CICD.
+
+## Notes
+
+- [CF stack](https://github.com/buildkite/elastic-ci-stack-for-aws)
---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    encrypt        = true
+    bucket         = "grocky-tfstate"
+    dynamodb_table = "tfstate-lock"
+    region         = "us-east-1"
+    key            = "buildkite/terraform.tfstate"
+  }
+}
+
+data "terraform_remote_state" "root" {
+  backend = "s3"
+  config = {
+    bucket = "grocky-tfstate"
+    region = "us-east-1"
+    key    = "rockygray.com/terraform.tfstate"
+  }
+}
+
