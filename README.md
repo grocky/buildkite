78466b63d998eda24104dda2450a66fba6b17ab4
Rocky Gray <rocky.grayjr@gmail.com>
2020-06-14 16:03:24 -0400

    use existing cloudformation template in terraform

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Testing out Buildkite CICD.
 
-## Notes
+## Overview
+
+The cloudformation stack creates the following resources:
+
+![](./cloudformation/diagram.png)
+
+We're wrapping the CloudFormation stack with Terraform to make it easier to integrate with existing infrastructure.
 
 - [CF stack](https://github.com/buildkite/elastic-ci-stack-for-aws)
+- Experiments: [agent experiments](https://github.com/buildkite/agent/blob/master/EXPERIMENTS.md)
--- main.tf
+++ main.tf
@@ -21,3 +21,76 @@ data "terraform_remote_state" "root" {
   }
 }
 
+variable "agent_token" {
+  description = "The buildkite agent token from the account in buildkite.com"
+}
+
+// sourced from: https://github.com/buildkite/elastic-ci-stack-for-aws/issues/246#issuecomment-399426091
+resource "aws_cloudformation_stack" "buildkite" {
+  name = "buildkite"
+
+  parameters = {
+    KeyName                                                   = aws_key_pair.buildkite.key_name
+    BuildkiteAgentRelease                                     = "stable"
+    BuildkiteOrgSlug                                          = "" # Set if there will be multiple buildkite orgs in the same AWS account
+    BuildkiteAgentToken                                       = var.agent_token
+    BuildkiteAgentTags                                        = "os=linux"
+    BuildkiteAgentTimestampLines                              = "false"
+    BuildkiteAgentExperiments                                 = ""
+    BuildkiteTerminateInstanceAfterJob                        = "false"
+    BuildkiteTerminateInstanceAfterJobTimeout                 = 1800
+    BuildkiteTerminateInstanceAfterJobDecreaseDesiredCapacity = "false"
+    BuildkiteAdditionalSudoPermissions                        = ""
+    BuildkiteQueue                                            = "rockygray-com"
+    AgentsPerInstance                                         = 4
+    SecretsBucket                                             = "" # auto create
+    ArtifactsBucket                                           = "" # auto create
+    BootstrapScriptUrl                                        = "" # auto create
+    AuthorizedUsersUrl                                        = ""
+    VpcId                                                     = "" # auto create
+    Subnets                                                   = "" # auto create
+    AvailabilityZones                                         = "" # auto create
+    InstanceType                                              = "c5.2xlarge"
+    SpotPrice                                                 = "0"
+    MaxSize                                                   = 3
+    MinSize                                                   = 0
+    ScaleUpAdjustment                                         = 1
+    ScaleDownAdjustment                                       = -1
+    ScaleCooldownPeriod                                       = 300
+    ScaleDownPeriod                                           = 1800
+    InstanceCreationTimeout                                   = "PT5M"
+    RootVolumeSize                                            = 10
+    RootVolumeName                                            = "/dev/xvda"
+    RootVolumeType                                            = "gp2"
+    SecurityGroupId                                           = ""
+    ImageId                                                   = "" # default
+    ManagedPolicyARN                                          = ""
+    InstanceRoleName                                          = ""
+    ECRAccessPolicy                                           = "none" # default
+    AssociatePublicIpAddress                                  = "true"
+    EnableSecretsPlugin                                       = "true"
+    EnableECRPlugin                                           = "false"
+    EnableDockerLoginPlugin                                   = "true"
+    EnableDockerUserNamespaceRemap                            = "true"
+    EnableDockerExperimental                                  = "false"
+    EnableCostAllocationTags                                  = "true"
+    CostAllocationTagName                                     = "App"
+    CostAllocationTagValue                                    = "buildkite"
+    EnableAgentGitMirrorsExperiment                           = "false"
+    EnableExperimentalLambdaBasedAutoscaling                  = "false"
+  }
+
+  //template_url  = "https://s3.amazonaws.com/buildkite-aws-stack/v${var.elastic_ci_stack_version}/aws-stack.json"
+  template_body = file("${path.module}/cloudformation/aws-stack.yml")
+
+  capabilities = ["CAPABILITY_IAM", "CAPABILITY_NAMED_IAM"]
+}
+
+variable "public_key" {
+  description = "The public key to allow EC2 ssh ingress."
+}
+
+resource "aws_key_pair" "buildkite" {
+  key_name   = "buildkite-ssh"
+  public_key = var.public_key
+}
